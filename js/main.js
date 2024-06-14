ee9e57948784cdc269185e806d9c041053063753
Yoana <yoanabarbie@icloud.com>
2024-06-14 13:51:46 +0200

    fixed code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-const bringCharacters = async () => {
-    const characters = await fetch('https://rickandmortyapi.com/api/character/?page=2');
-    const data = await characters.json();
+let prevPage = "";
+let nextPage = "";
 
-    const cardElements = data.results.map(person => {
-        return(`
+const bringCharacters = async (url) => {
+  const characters = await fetch(
+    `${url === "" ? "https://rickandmortyapi.com/api/character/?page=1" : url}`
+  );
+  const data = await characters.json();
+
+  prevPage = data.info.prev;
+  nextPage = data.info.next;
+
+  const cardElements = data.results.map((person) => {
+    return `
             <div class="col-md-4">
                 <div class="card mb-3">
                     <img src="${person.image}" class="card-img-top" alt="${person.id}">
@@ -12,15 +20,24 @@ const bringCharacters = async () => {
                     </div>
                 </div>
             </div>
-        `);
-    });
+        `;
+  });
 
-    document.getElementById("characters").innerHTML = `
+  document.getElementById("characters").innerHTML = `
         <div class="row">
             ${cardElements.join("")}
         </div>
     `;
-}
+};
 
-bringCharacters();
-    
+bringCharacters("");
+
+let buttons = document.getElementsByTagName("button");
+
+let buttonsArray = Array.from(buttons);
+
+buttonsArray.map((boton) =>
+  boton.addEventListener("click", (e) => {
+    e.target.id === "p" ? bringCharacters(!prevPage ? "" : prevPage) : bringCharacters(!nextPage ? "" : nextPage);
+  })
+);
